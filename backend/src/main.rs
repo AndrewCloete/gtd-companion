ddb539968bb78b2badb0eeccb285242d594a4ab7
andrew <user@gitmail.com>
2023-06-17 18:43:07 +0200

    Implements basic walkdir to extract all list items

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs;
+use walkdir::{DirEntry, WalkDir};
 
 mod model {
     #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -36,19 +38,52 @@ mod model {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// Root directory of the knowledge base
+    #[arg(short, long, value_hint = clap::ValueHint::DirPath)]
+    dir: std::path::PathBuf,
+
     /// Task status todo, wip, or review
     #[arg(short, long)]
-    status: model::TaskStatus,
+    status: Option<model::TaskStatus>,
 
     /// Number of times to greet
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
 
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
 fn main() {
     let args = Args::parse();
 
     for _ in 0..args.count {
-        println!("Hello {}!", args.status)
+        println!("Hello {}!", args.dir.display())
+    }
+    let file_paths = WalkDir::new(args.dir.as_path())
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .map(|e| e.path().display().to_string());
+
+    let lines: Vec<String> = file_paths
+        .flat_map(|file_path| {
+            fs::read_to_string(file_path)
+                .unwrap()
+                .lines()
+                .map(|line| String::from(line))
+                .filter(|line| line.starts_with("- ") || line.starts_with("* "))
+                .collect::<Vec<String>>()
+        })
+        .collect();
+
+    for line in lines {
+        println!("{}", line)
     }
 }
